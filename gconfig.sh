1b89e4736937aa376ac85eedf159750471179a60
toddcircle <155283690+toddcircle@users.noreply.github.com>
2025-07-19 16:16:53 +1000

    gconfig.sh

--- gconfig.sh
+++ gconfig.sh
@@ -2,3 +2,4 @@
 gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:maximize,minimize,close'
 gsettings set org.gnome.desktop.wm.preferences action-right-click-titlebar 'lower'
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
+gsettings set org.gnome.nautilus.preferences always-use-location-entry true
