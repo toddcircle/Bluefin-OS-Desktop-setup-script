650a5a7f9f74811f4607a10f2aa2712d764890e0
toddcircle <155283690+toddcircle@users.noreply.github.com>
2025-07-19 15:44:16 +1000

    Add files via upload

---
+++ gconfig.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,maximize:maximize,minimize,close'
+gsettings set org.gnome.desktop.wm.preferences action-right-click-titlebar 'lower'
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
