9971118b5bd3957a7a924248ad38d3ff49fefe22
toddcircle <155283690+toddcircle@users.noreply.github.com>
2025-07-27 14:47:54 +1000

    gconfig.sh

--- gconfig.sh
+++ gconfig.sh
@@ -6,9 +6,13 @@ gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 gsettings set org.gnome.desktop.interface enable-hot-corners true
+
+# Flatpak installs and dock icons
 flatpak info org.gnome.gedit >/dev/null 2>&1 || flatpak install -y flathub org.gnome.gedit
-gsettings set org.gnome.shell favorite-apps "$(python3 -c 'import json, sys; favs = json.loads(sys.stdin.read()); new = ["org.gnome.gedit.desktop"]; [favs.append(app) for app in new if app not in favs]; print(json.dumps(favs))' <<< "$(gsettings get org.gnome.shell favorite-apps | sed "s/'/\"/g")")"
+flatpak info com.brave.Browser >/dev/null 2>&1 || flatpak install -y flathub com.brave.Browser
 gsettings set org.gnome.shell favorite-apps "$(python3 -c 'import json, sys; favs = json.loads(sys.stdin.read()); new = ["gnome-calculator.desktop"]; [favs.append(app) for app in new if app not in favs]; print(json.dumps(favs))' <<< "$(gsettings get org.gnome.shell favorite-apps | sed "s/'/\"/g")")"
+gsettings set org.gnome.shell favorite-apps "$(python3 -c 'import json, sys; favs = json.loads(sys.stdin.read()); new = ["org.gnome.gedit.desktop"]; [favs.append(app) for app in new if app not in favs]; print(json.dumps(favs))' <<< "$(gsettings get org.gnome.shell favorite-apps | sed "s/'/\"/g")")"
+gsettings set org.gnome.shell favorite-apps "$(python3 -c 'import json, sys; favs = json.loads(sys.stdin.read()); new = ["com.brave.Browser.desktop"]; [favs.append(app) for app in new if app not in favs]; print(json.dumps(favs))' <<< "$(gsettings get org.gnome.shell favorite-apps | sed "s/'/\"/g")")"
 
 # Symlink wallpapers to backgrounds for use with Linux Theme Store
 mkdir -p ~/.local/share/backgrounds
