ddf6ae007f712fad62e3d2b0488a4295fd057635
toddcircle <155283690+toddcircle@users.noreply.github.com>
2025-07-27 11:11:48 +1000

    gconfig.sh

--- gconfig.sh
+++ gconfig.sh
@@ -3,3 +3,7 @@ gsettings set org.gnome.desktop.wm.preferences button-layout 'close,minimize,max
 gsettings set org.gnome.desktop.wm.preferences action-right-click-titlebar 'lower'
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+
+# Symlink wallpapers to backgrounds for use with Linux Theme Store
+rm -rf ~/.local/share/wallpapers
+ln -s ~/.local/share/backgrounds ~/.local/share/wallpapers
