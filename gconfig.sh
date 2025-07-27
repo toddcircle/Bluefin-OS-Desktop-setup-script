3dc0e92850cce9def7e51606cf441e25a9d4a9d3
toddcircle <155283690+toddcircle@users.noreply.github.com>
2025-07-27 14:01:26 +1000

    gconfig.sh

--- gconfig.sh
+++ gconfig.sh
@@ -4,6 +4,7 @@ gsettings set org.gnome.desktop.wm.preferences action-right-click-titlebar 'lowe
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll true
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
 
 # Symlink wallpapers to backgrounds for use with Linux Theme Store
 mkdir -p ~/.local/share/backgrounds
